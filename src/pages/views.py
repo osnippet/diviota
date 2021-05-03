93b55c3ccb833f4ce0d8253c93882cbdc382943a
Osnippet <46687941+osnippet@users.noreply.github.com>
2021-05-02 22:31:56 -0700

    copy of docker django example

---
+++ src/pages/views.py
@@ -0,0 +1,25 @@
+import os
+
+from django import get_version
+from django.conf import settings
+from django.db import connection
+from django.http import HttpResponse
+from django.shortcuts import render
+from django_redis import get_redis_connection
+
+
+def home(request):
+    context = {
+        "debug": settings.DEBUG,
+        "django_ver": get_version(),
+        "python_ver": os.environ["PYTHON_VERSION"],
+    }
+
+    return render(request, "pages/home.html", context)
+
+
+def up(request):
+    get_redis_connection().ping()
+    connection.ensure_connection()
+
+    return HttpResponse("")
